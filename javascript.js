9054324e2b9e5d8367de63f2a788c17a46437bd5
Ital0Alve5 <italo_alves_@outlook.com>
2020-09-18 19:02:10 -0300

    Adicionado submenus e toda a sua animação. Mudança nas cores dos fundos dos tópicos ao passar o mouse.

--- javascript.js
+++ javascript.js
@@ -11,9 +11,56 @@ function fixarBarraDeNavegação(){
         barraFixa.style.left = 0
         barraFixa.style.right = 0
         barraFixa.style.overflow = 'hidden'
-        barraFixa.style.zIndex = 999}
-    else{barraFixa.style.display = 'none'}
+        barraFixa.style.zIndex = 999
 }
+    else{barraFixa.style.display = 'none'
+    document.querySelector(".containersubtópicosFixo").style.display = "none"
+}
+}
+
+/*------------------------------------------SUBMENU----------------------------------------------*/
+
+function displayFixo(){ //Faz o submenu da barra fixa funcionar
+    document.querySelector(".containersubtópicosFixo").style.display = "flex"
+    document.querySelector(".containersubtópicosFixo").style.position = "fixed"
+    document.querySelector(".containersubtópicosFixo").style.top = "50px"
+    document.querySelector(".containersubtópicosFixo").style.right = "31%"
+}
+
+function display(){ //faz o submenu da barra inicial funcionar
+    if(window.innerWidth > 700){ //resolve o bug dela aparecer no modo mobile
+    document.querySelector(".containersubtópicos").style.display = "flex"
+    document.querySelector(".containersubtópicos").style.position = "absolute"
+    document.querySelector(".containersubtópicos").style.top = "77px"
+    document.querySelector(".containersubtópicos").style.right = "25%"
+    }
+}
+var contador = 0
+function displayMobile(){ //faz o submenu mobile funcionar
+    if(window.innerWidth < 700){
+        contador++
+        if(contador % 2 !== 0){ //O submenu mobile só funciona clicando, então esta função faz com que ele apareça ao clicar...
+            document.querySelector(".containerSubMobile").style.display = 'flex'
+        }
+    else {document.querySelector(".containerSubMobile").style.display = 'none'} //...e suma ao clicar novamente
+    } 
+}
+
+function hide(){ //faz com que, ao tirar o mouse do submenu, ele suma
+    document.querySelector(".containersubtópicos").style.display = "none"
+    document.querySelector(".containersubtópicosFixo").style.display = 'none'
+}
+
+var subtópicoMobile =  document.querySelectorAll('.subtópicoMobile')
+
+subtópicoMobile.forEach(subtópico => {subtópico.addEventListener('click', fecharMenu)})
+function fecharMenu(){ 
+    var checkbox = document.getElementById('barraMenu')
+    checkbox.checked = false //faz com que, ao clicar em algum dos subtópicos, o menu suba
+    document.querySelector('.containerSubMobile').style.display = 'none' //resolve o bug do submenu não ficar visível ao menu subir
+    contador++ //resolve o bug de ter que clicar duas vezes da próxima vez que for abrir o submenu
+}
+
 
 /*---------------------------------------------------------ROLA PARA A SECTION ESPECÍFICA-------------------------------------------------------------------*/
 
@@ -23,11 +70,14 @@ menu.forEach(paraCadaitem =>{paraCadaitem.addEventListener('click', ScrollToId)}
 function ScrollToId(event){ //O parâmetro nos mostra que houve um evento de mouse e apresenta uma série de métodos.    
     event.preventDefault() //Essa função impede que o javascript execute uma função padrão do evento clicado. Neste caso, um <a> é clcado e esta função
     //previne que o html coloque o valor do href na URL e bloqueia que a página vá para a seção com o id que está no href    
-    var checkbox = document.getElementById('barraMenu') //Pegar o checkbox do botão de menu na versão mobile    
-    checkbox.checked = false //Sempre que, na versão mobile, uma opção for clicada, o checkbox é desmarcado e o menu sobe automaticamente    
+    //var checkbox = document.getElementById('barraMenu') //Pegar o checkbox do botão de menu na versão mobile    
+    //checkbox.checked = false //Sempre que, na versão mobile, uma opção for clicada, o checkbox é desmarcado e o menu sobe automaticamente    
     document.getElementById('icone').src = "img/iconeMenu.png" //Ao clicar em alguma opção de navegação do menu mobile, o ícone de menu que está um "X", volta a ser 3 barras horizontais
     const items = event.target //especifica um dos métodos que quero utilizar no evento de mouse. Este método mostra qual foi o elemento html que foi clicado    
     const id = items.getAttribute('href') //Estou pegando o valor do atributo href , que é um id, do elemento que  foi clicado e colocando na constante "id" para colocar o id dos elementos como parâmetro    
+    
+    if(id !== "#Serviços"){var checkbox = document.getElementById('barraMenu') //Pegar o checkbox do botão de menu na versão mobile    
+    checkbox.checked = false} //Sempre que, na versão mobile, uma opção for clicada, o checkbox é desmarcado e o menu sobe automaticamente
     const section = document.querySelector(id).offsetTop //Como disse acima, estou colocando o valor do href, que é um id, como parâmetro para que o query peque a section com o id específico.
     //a propriedade offsetTop retorna quantos píxels o topo do elemento está do topo da página, ou seja, em que altura do documento o elemento está localizado.    
     window.scroll({top: section - 50, behavior: "smooth"}) //este método do window rola para as coordenadas ditas no parâmetro. Neste caso, o parâmetro foi um objeto
@@ -69,6 +119,7 @@ function mudarIcone(){
 }
 
 /*-----------------------------------SETA IMAGEM PRO MODAL AO CLICAR--------------------------------------------------------------------------------------------------------*/
+
 var imagemModal = document.querySelector('#imagemModal')
 var modal = document.querySelector('.modal')
 var imagensDaGaleria = document.querySelectorAll('.fotosDaGaleria')
